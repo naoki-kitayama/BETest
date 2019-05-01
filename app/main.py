c9f35ff651523bcfafaeddb6540fee5ec30d6fcf
naoki-kitayama <fenrir_north@yahoo.co.jp>
2019-05-01 22:06:39 +0900

    change message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ api = responder.API()
 
 @api.route("/")
 def hello_world(req, resp):
-	resp.text = "hello, world!"
+	resp.text = "Happy new era!"
 
 if __name__ == '__main__':
 	api.run(address="0.0.0.0",port=80)
