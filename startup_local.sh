730ac3380a897b6910b0a4e0793ce7e4c52a22a2
naoki-kitayama <fenrir_north@yahoo.co.jp>
2019-05-02 20:06:23 +0900

    supervisor for nginx start

--- startup_local.sh
+++ startup_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # nginxの設定を変更
-sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf
+sed -i -e 's/$PORT/'"80"'/g' /etc/nginx/conf.d/default.conf
 
 # アプリケーションを実行
 supervisord -c /etc/supervisord.conf
